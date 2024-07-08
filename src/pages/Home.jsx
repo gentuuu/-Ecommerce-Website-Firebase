ad8cee9ea3b4fc47888455878c52e5de13870903
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-07-08 19:28:33 +0200

    section popular

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,6 +22,7 @@ const Home = () => {
   const [bestSalesProducts, setBestSalesProducts] = useState([]);
   const [mobileProducts, setMobileProducts] = useState([]);
   const [wirelessProducts, setWirelessProducts] = useState([]);
+  const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(()=>{
     const filteredTrendingProducts = products.filter(
@@ -40,10 +41,15 @@ const Home = () => {
       (item) => item.category === 'wireless'
     );
 
+    const filteredPopularProducts = products.filter(
+      (item) => item.category === 'watch'
+    );
+
     setTrendingProducts(filteredTrendingProducts);
     setBestSalesProducts(filteredBestSalesProducts);
     setMobileProducts(filteredMobileProducts);
     setWirelessProducts(filteredWirelessProducts);
+    setPopularProducts(filteredPopularProducts);
   },[]);
 
 
@@ -116,14 +122,25 @@ const Home = () => {
         </Container>
       </section>
 
-      <section className="nre__arrivals">
+      <section className="new__arrivals">
         <Container>
           <Row>
-            <Col lg='12' className="text-center">
+            <Col lg='12' className="text-center mb-5">
               <h2 className="section__title">New Arrivals</h2>
             </Col>
             <ProductsList data={mobileProducts} />
             <ProductsList data={wirelessProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className="popular__category">
+        <Container>
+          <Row>
+            <Col lg='12' className="text-center mb-5">
+              <h2 className="section__title">Popular in Category</h2>
+            </Col>
+            <ProductsList data={popularProducts} />
           </Row>
         </Container>
       </section>
