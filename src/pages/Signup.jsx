6aba7f9c445a6a08cbd87b253d449f4380d35201
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-07-30 16:33:59 +0200

    Add user do database
    Dodanie użytkownika przez formularz do bazy danych

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import '../styles/Login.scss'
 import { useState } from 'react'
 import { Container, Row, Col, Form, FormGroup } from "reactstrap"
 import Helmet from '../components/Helmet/Helmet'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
@@ -23,6 +23,8 @@ const Signup = () => {
   const [file, setFile] = useState(null)
   const [loading, setLoadnig] = useState(false)
 
+  const navigate = useNavigate()
+
   const signup = async(e)=>{
     e.preventDefault()
     setLoadnig(true)
@@ -40,18 +42,30 @@ const Signup = () => {
           toast.error(error.message);
         },
         () => {
-          getDownloadURL(uploadTask.snapshot.ref).then(async(getDownloadURL) =>{
+          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) =>{
+            //update user profile
             await updateProfile(user, {
               displayName: username,
-              photoURL: getDownloadURL,
+              photoURL: downloadURL,
             });
+
+            // store user data in firestore database
+            await setDoc(doc(db, 'users', user.uid),{
+              uid: user.uid,
+              displayName: username,
+              email,
+              photoUrl: downloadURL,
+            });  
           });
         }
       );
 
-      console.log(user)
+      setLoadnig(false)
+      toast.success('Account created')
+      navigate('/login')
     
     } catch (error){
+      setLoadnig(false)
       toast.error('something went wrong')
     }
 
@@ -62,28 +76,36 @@ const Signup = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='6' className='m-auto text-center'>
-              <h3 className="fw-bold fs-4">Signup</h3>
-              <Form className='auth__form' onSubmit={signup}>
-                <FormGroup className='form__group'>
-                  <input type="text" placeholder='UserName'
-                  value={username}  onChange={e=> setUsername(e.target.value)}/>
-                </FormGroup>
-                <FormGroup className='form__group'>
-                  <input type="email" placeholder='Enter your email'
-                  value={email}  onChange={e=> setEmail(e.target.value)}/>
-                </FormGroup>
-                <FormGroup className='form__group'>
-                  <input type="password" placeholder='Enter your password'
-                  value={password}  onChange={e=> setPassword(e.target.value)} />
-                </FormGroup>
-                <FormGroup className='form__group'>
-                  <input type="file" onChange={e=> setFile(e.target.files[0])} />
-                </FormGroup>
-                <button type='submit' className="buy__btn login__btn">Create an account</button>
-                <p>Already have an account? <Link to='/login'>Login</Link></p>
-              </Form>
-            </Col>
+            {
+              loading? (
+              <Col lg='12' className='text-center'>
+                <h5 className="fw-bold">Loading....</h5> 
+              </Col>
+              ) : (
+              <Col lg='6' className='m-auto text-center'>
+                <h3 className="fw-bold fs-4">Signup</h3>
+                <Form className='auth__form' onSubmit={signup}>
+                  <FormGroup className='form__group'>
+                    <input type="text" placeholder='UserName'
+                    value={username}  onChange={e=> setUsername(e.target.value)}/>
+                  </FormGroup>
+                  <FormGroup className='form__group'>
+                    <input type="email" placeholder='Enter your email'
+                    value={email}  onChange={e=> setEmail(e.target.value)}/>
+                  </FormGroup>
+                  <FormGroup className='form__group'>
+                    <input type="password" placeholder='Enter your password'
+                    value={password}  onChange={e=> setPassword(e.target.value)} />
+                  </FormGroup>
+                  <FormGroup className='form__group'>
+                    <input type="file" onChange={e=> setFile(e.target.files[0])} />
+                  </FormGroup>
+                  <button type='submit' className="buy__btn login__btn">Create an account</button>
+                  <p>Already have an account? <Link to='/login'>Login</Link></p>
+                </Form>
+              </Col>
+              )
+            }
           </Row>
         </Container>
       </section>
