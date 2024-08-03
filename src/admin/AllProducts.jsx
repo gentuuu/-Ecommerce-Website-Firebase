1d86f4a817a57ee4af2852a85e6e2fb04ce3481f
gentuuu <60502683+gentuuu@users.noreply.github.com>
2024-08-03 15:22:03 +0200

    Add list product dashboard

--- src/admin/AllProducts.jsx
+++ src/admin/AllProducts.jsx
@@ -1,8 +1,48 @@
+import { Container, Row, Col } from "reactstrap"
+import useGetData from "../custom-hooks/useGetData"
 
 
 const AllProducts = () => {
+
+  const {data: productsData, loading} = useGetData('products')
+
+  console.log(productsData)
+
   return (
-    <div>AllProducts</div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Category</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {loading ? (
+                  <h4 className="py-5 text-center fw-bold">Loading....</h4>
+                ) : (
+                  productsData.map(item=>(
+                    <tr key={item.id}>
+                      <td><img src={item.imgUrl} alt="" /></td>
+                      <td>{item.title}</td>
+                      <td>{item.category}</td>
+                      <td>{item.price}</td>
+                      <td><button className="btn btn-danger">Delete</button></td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
